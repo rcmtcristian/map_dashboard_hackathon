55305265ed250e5c4673f2af9beb4b7c87616212
Cris <rcmtcristian@gmail.com>
2025-08-13 23:05:06 -0400

    Add responsive navigation bar with logo and menu 87
    ## Description
    
    This PR implements a responsive navigation bar using Vuetify 3, featuring:
    - Logo that changes size for desktop and mobile
    - Desktop menu links aligned right
    - Mobile hamburger menu with specified color and size
    - Fixed 72px white nav bar height
    
    
    Tested locally and meets the project requirements.
    
    
    
    
    ## Type of Change
    
    - [ ] Bug fix
    - [x ] New feature
    - [ ] Documentation update
    - [ ] Refactor
    - [ ] Other (please describe):
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Include details of your testing environment if relevant.
    
    ## Checklist:
    
    - [ ] My code follows the style guidelines of this project
    - [x ] I have performed a self-review of my code
    - [ ] I have commented my code where necessary
    - [ ] I have added tests that prove my fix is effective or that my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [x ] I have updated the documentation (if applicable)
    
    ## Related Issues
    
    Fixes #<issue-number> (if applicable)

--- map-dashboard-ui/src/App.vue
+++ map-dashboard-ui/src/App.vue
@@ -1,5 +1,69 @@
 <template>
   <v-app>
+    <!-- Nav Bar -->
+    <v-container
+      class="d-flex align-center"
+      style="height: 72px; background-color: #FFFFFF"
+      fluid
+    >
+      <!-- Logo -->
+      <v-col
+        :cols="$vuetify.display.mdAndUp ? 6 : 10"
+        class="d-flex align-center"
+      >
+        <v-img
+          src="@/assets/logo.svg"
+          alt="PE Logo"
+          :height="$vuetify.display.mdAndUp ? '36' : '48'"
+          contain
+        />
+      </v-col>
+
+      <!-- Desktop Menu -->
+      <v-col
+        v-if="$vuetify.display.mdAndUp"
+        cols="6"
+        class="d-flex justify-end align-center"
+      >
+        <v-btn text>Home</v-btn>
+        <v-btn text>About</v-btn>
+        <v-btn text>Contact</v-btn>
+      </v-col>
+
+      <!-- Mobile Menu Button -->
+      <v-col v-else cols="2" class="d-flex justify-end align-center">
+        <v-btn
+          icon
+          size="36"
+          style="color: #33691E"
+          @click="drawer = true"
+        >
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </v-col>
+    </v-container>
+
+    <!-- Mobile Drawer -->
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      location="right"
+      style="background-color: #fff"
+    >
+      <v-list>
+        <v-list-item @click="drawer = false">
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="drawer = false">
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="drawer = false">
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Main -->
     <v-main>
       <v-container class="pa-10">
         <v-alert type="success" title="Vuetify 3 Installed!">
@@ -11,6 +75,7 @@
 </template>
 
 <script setup>
-// Nothing yet
-</script>
+import { ref } from 'vue'
 
+const drawer = ref(false)
+</script>
