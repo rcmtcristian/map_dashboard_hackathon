9b2d9beefdb0d40ccb78b7fa42483373341a8128
Cris <rcmtcristian@gmail.com>
2025-08-13 23:30:25 -0400

    Update App.vue

--- map-dashboard-ui/src/App.vue
+++ map-dashboard-ui/src/App.vue
@@ -1,120 +1,135 @@
 <template>
   <v-app>
+    <!-- Navigation Bar -->
+    <v-container
+      fluid
+      class="d-flex align-center"
+      style="height: 72px; background-color: #FFFFFF"
+    >
+      <!-- Logo -->
+      <v-col :cols="$vuetify.display.mdAndUp ? 6 : 10" class="d-flex align-center">
+        <v-img
+          src="@/assets/logo.svg"
+          alt="PE Logo"
+          :height="$vuetify.display.mdAndUp ? 36 : 48"
+          contain
+        />
+      </v-col>
+      
+      <!-- Desktop Menu -->
+      <v-col
+        v-if="$vuetify.display.mdAndUp"
+        cols="6"
+        class="d-flex justify-end align-center"
+      >
+        <v-btn text>Home</v-btn>
+        <v-btn text>About</v-btn>
+        <v-btn text>Contact</v-btn>
+      </v-col>
+      
+      <!-- Mobile Hamburger -->
+      <v-col v-else cols="2" class="d-flex justify-end align-center">
+        <v-btn
+          icon
+          size="36"
+          style="color: #33691E"
+          @click="drawer = true"
+        >
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </v-col>
+    </v-container>
 
-    <!------------ Nav Bar --------------->
-    <v-toolbar>
-      Nav bar goes here
-    </v-toolbar> <!-- /end Nav Bar-->
+    <!-- Mobile Drawer -->
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      location="right"
+      style="background-color: #fff"
+    >
+      <v-list>
+        <v-list-item @click="drawer = false"><v-list-item-title>Home</v-list-item-title></v-list-item>
+        <v-list-item @click="drawer = false"><v-list-item-title>About</v-list-item-title></v-list-item>
+        <v-list-item @click="drawer = false"><v-list-item-title>Contact</v-list-item-title></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-    <!-- Main Content -->
+    <!-- Main Content & Map Layout -->
     <v-main>
-
-      <!-- Grid layout -->
       <v-container>
         <v-row>
-
-          <!--------- Map Visualization -------->
-          <v-col cols="12" md="8" style="border: 1px solid black;" class="map-vis-col pa-0">
+          <!-- Map Visualization -->
+          <v-col cols="12" md="8" class="map-vis-col pa-0" style="border: 1px solid black;">
             <v-sheet>
-              <!-- Test image just as a placeholder -->
-               <v-img
-                  cover
-                  src="/images/Map_Screenshot_Vue_Test.jpg">
-                </v-img>
+              <v-img cover src="/images/Map_Screenshot_Vue_Test.jpg" />
             </v-sheet>
-          </v-col> <!-- /end Map Visualization -->
-
-          <!-------------- Map Information -------------->
+          </v-col>
+          
+          <!-- Map Info -->
           <v-col cols="12" md="4" class="map-info-col">
-
-            <v-sheet class="map-info-reset"> 
-
-              <!-- Title -->
+            <v-sheet class="map-info-reset">
               <h1 class="text-h1 mb-6">Biodiversity Conservation</h1>
 
-              <!-- Maps section -->
+              <!-- Maps Section -->
               <v-container class="pa-0 mb-6">
                 <h2 class="text-h2 mb-4">Maps</h2>
-                <p class="text-body-1">Select to view map layer:</p>
-                <!-- Checkboxes to select map layers -->
-                <v-checkbox label="ProgramEarth Conservation Partners" density="compact" hide-details></v-checkbox>
-                <v-checkbox label="USGS Watershed Boundary Dataset" density="compact" hide-details></v-checkbox>
-                <v-checkbox label="Meta's Canopy Heights" density="compact" hide-details></v-checkbox>                
-              </v-container> <!--/end maps section-->
-              
+                <p>Select to view map layer:</p>
+                <v-checkbox label="ProgramEarth Conservation Partners" density="compact" hide-details />
+                <v-checkbox label="USGS Watershed Boundary Dataset" density="compact" hide-details />
+                <v-checkbox label="Meta's Canopy Heights" density="compact" hide-details />
+              </v-container>
+
               <!-- Legend -->
               <v-container class="pa-0 mb-6">
                 <h2 class="text-h2 mb-4">Legend</h2>
-
-                <!-- Points -->
+                
                 <div class="points-container">
                   <h3 class="text-h3 mb-3">Points</h3>
-                  <!-- Landmark -->
                   <v-container class="points-item mb-3">
-                    <i class="fas fa-circle" style="color: #424242; font-size: 16px;"></i> <p class="text-body-1">Landmark</p> <!-- /end Landmark -->
+                    <i class="fas fa-circle" style="color: #424242; font-size: 16px;"></i>
+                    <p>Landmark</p>
                   </v-container>
-                  <!-- Animals -->
                   <v-container class="points-item mb-3">
-                    <i class="fas fa-dove" style="color: #2196F3; font-size: 16px"></i> <p class="text-body-1">Animals</p>
-                  </v-container> <!-- /end Animals -->
-                  <!-- Plants -->
+                    <i class="fas fa-dove" style="color: #2196F3; font-size: 16px;"></i>
+                    <p>Animals</p>
+                  </v-container>
                   <v-container class="points-item">
-                    <i class="fas fa-seedling" style="color: #43A047; font-size: 16px;"></i> <p class="text-body-1">Plants</p> <!-- /end plants -->
-                  </v-container> 
-                </div> <!-- /end points -->
+                    <i class="fas fa-seedling" style="color: #43A047; font-size: 16px;"></i>
+                    <p>Plants</p>
+                  </v-container>
+                </div>
 
-                <!-- Areas -->
                 <div class="areas-container">
-                  <h3 class="text-h3 mb-3">Areas</h3> 
-                  <!-- Species -->
+                  <h3 class="text-h3 mb-3">Areas</h3>
                   <v-container class="areas-item mb-3" style="width: fit-content;">
-                    <v-img
-                      :width="30"
-                      :height="30"
-                      src="/images/species_icon.svg"
-                    ></v-img>
-                    <p class="text-body-1">Species</p>
-                  </v-container> <!-- /end species -->
-                  <!-- Water -->
+                    <v-img width="30" height="30" src="/images/species_icon.svg" />
+                    <p>Species</p>
+                  </v-container>
                   <v-container class="areas-item mb-3" style="width: fit-content;">
-                    <v-img
-                      :width="30"
-                      :height="30"
-                      src="/images/water_polygon_icon.svg"
-                    ></v-img>
-                    <p class="text-body-1">Water</p>
-                  </v-container> <!-- /end water -->
-                  <!-- Canopy -->
+                    <v-img width="30" height="30" src="/images/water_polygon_icon.svg" />
+                    <p>Water</p>
+                  </v-container>
                   <v-container class="areas-item" style="width: fit-content;">
-                    <v-img
-                      :width="30"
-                      :height="30"
-                      src="/images/canopy_polygon_icon.svg"
-                    ></v-img>
-                    <p class="text-body-1">Canopy</p>
-                  </v-container> <!-- /end canopy -->
-                </div> <!-- /end Areas -->
-                
-              </v-container> <!-- /end Legend -->
-
-            </v-sheet> 
-
-          </v-col> <!-- /end Map Information -->
-
+                    <v-img width="30" height="30" src="/images/canopy_polygon_icon.svg" />
+                    <p>Canopy</p>
+                  </v-container>
+                </div>
+              </v-container>
+            </v-sheet>
+          </v-col>
         </v-row>
-      </v-container> <!-- /end grid layout -->
-
+      </v-container>
     </v-main>
 
-    <!---------------- Footer ------------------->
+    <!-- Footer -->
     <v-footer>
       Footer goes here
     </v-footer>
-
   </v-app>
 </template>
 
 <script setup>
-// Nothing yet
-</script>
+import { ref } from 'vue'
 
+const drawer = ref(false)
+</script>
